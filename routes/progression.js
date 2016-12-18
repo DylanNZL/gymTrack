ff7090505430e419ff1970015030c367bfd31917
Dylan Cross <dylnnzl@gmail.com>
2016-12-18 15:56:01 +1300

    Workout/Progress rendering

--- routes/progression.js
+++ routes/progression.js
@@ -1,3 +1,12 @@
 /**
  * Created by Dylan on 18-Dec-16.
  */
+var express = require('express');
+var router = express.Router();
+
+/* GET progression/stats page. */
+router.get('/', function(req, res, next) {
+    res.render('progression', { title: 'Exercise Tracker' });
+});
+
+module.exports = router;
--- routes/workout.js
+++ routes/workout.js
@@ -1,3 +1,12 @@
 /**
  * Created by Dylan on 18-Dec-16.
  */
+var express = require('express');
+var router = express.Router();
+
+/* GET workout page. */
+router.get('/', function(req, res, next) {
+    res.render('workout', { title: 'Exercise Tracker' });
+});
+
+module.exports = router;
