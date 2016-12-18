2ab152bc9d9f9fe2df61fb9ea564191d44133ba1
Dylan Cross <dylnnzl@gmail.com>
2016-12-18 19:15:09 +1300

    Form to choose which exercise to display

--- routes/progression.js
+++ routes/progression.js
@@ -2,11 +2,23 @@
  * Created by Dylan on 18-Dec-16.
  */
 var express = require('express');
+var database = require('../db/database.js');
 var router = express.Router();
 
 /* GET progression/stats page. */
 router.get('/', function(req, res, next) {
-    res.render('progression', { title: 'Exercise Tracker' });
+    rend(res);
 });
 
+function rend(res) {
+    //database.getExerciseHistoryFromDate();
+    database.getExercises(function (data) {
+        if (data) {
+            res.render('progression', { title: 'Exercise Tracker', exercise : data });
+        } else {
+            res.render('progression', {title: 'Exercise Tracker', exercise: 0});
+        }
+    });
+
+}
 module.exports = router;
