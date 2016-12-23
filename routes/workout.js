c38f447a08c7e172e088f52ca87bef4dc9dd8231
Dylan Cross <dylnnzl@gmail.com>
2016-12-23 16:30:01 +1300

    Server takes down current set but overwrites all of the saved, will have to fix that, also switching computers

--- routes/workout.js
+++ routes/workout.js
@@ -5,10 +5,7 @@ var express = require('express');
 var database = require('../db/database.js');
 var router = express.Router();
 
-var prevTemplate = {
-    name : "",
-    set : []
-};
+var exercises = 0;
 
 var setTemplate = {
     setNo : 0,
@@ -22,49 +19,72 @@ var currentSets = {
 };
 
 var currentSetNo = 1;
-var prev = [];
 
-/* GET workout page. */
+// GET workout page.
+// Based on the query it will render the base page, or update the current workout based on what variables are sent
 router.get('/', function(req, res, next) {
+    //console.log(req.query);
     var reps = req.query.reps;
     var weight = req.query.weight;
-    var newEx = req.query.newEx;
+    var newEx = req.query.exercise;
+    //console.log(reps + ' ' + weight + ' ' + newEx);
     if (reps && weight) { capture(res, reps, weight); }
     else if (newEx) { newExercise(res, newEx) }
     else { rend(res); }
 
 });
 
-function capture(res) {
+// Add new set to the current group for this exercise, render page again
+function capture(res, reps, weight) {
+    console.log("capture");
+    console.log(currentSets);
     var set = setTemplate;
     set.setNo = currentSetNo;
     set.reps = reps;
     set.weight = weight;
     currentSets.sets.push(set);
+    currentSetNo++;
     rend (res);
 }
 
+// Start new exercise, clear current group, render page again
 function newExercise(res, newEx) {
+    console.log("new ex");
+    // Add current to the previous array
+    var addPrev = prevTemplate;
+    addPrev.name = currentSets.name;
+    addPrev.set = currentSets.sets;
+    prev.push(addPrev);
+    // Reset current array
     currentSets.name = newEx;
     currentSets.sets = [];
     currentSetNo = 1;
     rend(res);
 }
 
+// Render page, pull all exercises in the db for the drop down menu
 function rend (res) {
-    database.getExercises(function (data) {
-        if (data) {
-            res.render('workout', {
-                title : 'Exercise Tracker',
-                exercise : data,
-                current : currentSets,
-                currentSet : currentSetNo,
-                previous : prev
-            });
-        } else {
-
-        }
-    });
+    if (exercises === 0) {
+        database.getExercises(function (data) {
+            if (data) {
+                exercises = 0;
+                res.render('workout', {
+                    title: 'Exercise Tracker',
+                    exercise: data,
+                    current: currentSets,
+                    currentSet: currentSetNo
+                });
+            }
+        });
+    } else {
+        res.render('workout', {
+            title: 'Exercise Tracker',
+            exercise: data,
+            current: currentSets,
+            currentSet: currentSetNo,
+            previous: prev
+        });
+    }
 }
 
 module.exports = router;
