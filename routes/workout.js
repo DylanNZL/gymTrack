d9288e9fec164e64f5e3b9b1846196fb54510b0c
Dylan Cross <dylancross@me.com>
2016-12-24 13:20:36 +1300

    switch computers

--- routes/workout.js
+++ routes/workout.js
@@ -14,16 +14,27 @@ var currentSetNo = 1;
 
 // GET workout page.
 // Based on the query it will render the base page, or update the current workout based on what variables are sent
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     var reps = req.query.reps;
     var weight = req.query.weight;
     var newEx = req.query.exercise;
+    var startNew = req.query.startNew;
 
     if (reps && weight) { capture(res, reps, weight); }
-    else if (newEx) { newExercise(res, newEx) }
+    else if (newEx) { newExercise(res, newEx); }
+    else if (startNew) { initialiseNewWorkout(res); }
     else { rend(res); }
 
 });
+
+function initialiseNewWorkout(res) {
+    // Initialises variables again
+    currentSets = [];
+    currentName = "Select Workout";
+    currentSetNo = 1;
+    exercises = 0;
+    rend(res);
+}
 
 // Add new set to the current group for this exercise, render page again
 function capture(res, reps, weight) {
@@ -43,7 +54,11 @@ function capture(res, reps, weight) {
 // Start new exercise, clear current group, render page again
 function newExercise(res, newEx) {
     console.log("new ex");
+    // save current set to database
+    database.addNewSet();
+
     // Reset current array
+
     currentSets.name = newEx;
     currentSets.sets = [];
     currentSetNo = 1;
